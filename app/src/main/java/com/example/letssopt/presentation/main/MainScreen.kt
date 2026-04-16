9245df8bebf8da4bf0a6dff6deadf037618ad20c
seungjae708 <choesj365@gmail.com>
2026-04-16 16:10:28 +0900

    feat: MainScreen 구현

---
+++ app/src/main/java/com/example/letssopt/presentation/main/MainScreen.kt
@@ -0,0 +1,52 @@
+package com.example.letssopt.presentation.main
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.letssopt.core.designsystem.theme.LETSSOPTTheme
+import com.example.letssopt.presentation.home.HomeScreen
+import com.example.letssopt.presentation.library.LibraryScreen
+import com.example.letssopt.presentation.main.component.MainBottomBar
+import com.example.letssopt.presentation.purchase.PurchaseScreen
+import com.example.letssopt.presentation.search.SearchScreen
+import com.example.letssopt.presentation.webtoon.WebtoonScreen
+import kotlinx.collections.immutable.toImmutableList
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+    var currentTab by remember { mutableStateOf(MainTab.MAIN) }
+
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        bottomBar = {
+            MainBottomBar(
+                isVisible = true,
+                tabs = MainTab.entries.toImmutableList(),
+                currentTab = currentTab,
+                onTabSelected = { currentTab = it }
+            )
+        }
+    ) { innerPadding ->
+        when (currentTab) {
+            MainTab.MAIN -> HomeScreen(paddingValues = innerPadding)
+            MainTab.PURCHASE -> PurchaseScreen(paddingValues = innerPadding)
+            MainTab.WEBTOON -> WebtoonScreen(paddingValues = innerPadding)
+            MainTab.SEARCH -> SearchScreen(paddingValues = innerPadding)
+            MainTab.LIBRARY -> LibraryScreen(paddingValues = innerPadding)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    LETSSOPTTheme {
+        MainScreen()
+    }
+}
