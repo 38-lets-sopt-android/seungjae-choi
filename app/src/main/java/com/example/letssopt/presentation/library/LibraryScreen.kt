605cdc7c7b07e2f267a19432c5dddda20a41f05f
seungjae708 <choesj365@gmail.com>
2026-04-16 16:11:27 +0900

    feat: 홈, 개별구매, 웹툰, 찾기, 보관함 스크린 추가

---
+++ app/src/main/java/com/example/letssopt/presentation/library/LibraryScreen.kt
@@ -0,0 +1,37 @@
+package com.example.letssopt.presentation.library
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LibraryRoute(
+    paddingValues: PaddingValues
+) {
+    LibraryScreen(
+        paddingValues = paddingValues
+    )
+}
+
+@Composable
+fun LibraryScreen(
+    paddingValues: PaddingValues
+) {
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Library"
+        )
+    }
+}
