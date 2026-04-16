a3aa8b0ad2c671a40c248e8008296b6c5b596127
seungjae708 <choesj365@gmail.com>
2026-04-16 20:05:46 +0900

    feat: HomeContract 더미 데이터 추가

--- app/src/main/java/com/example/letssopt/presentation/home/state/HomeContract.kt
+++ app/src/main/java/com/example/letssopt/presentation/home/state/HomeContract.kt
@@ -22,8 +22,12 @@ data class HomeUiState(
             R.drawable.img_content2,
             R.drawable.img_content3,
             R.drawable.img_content1,
-            )
+            R.drawable.img_content2,
+            R.drawable.img_content3,
+        )
         val dummyPartyList = listOf(
+            PartyUiModel(R.drawable.img_party, "오늘 21:13에 시작", "# 왕과 사는 남자"),
+            PartyUiModel(R.drawable.img_party, "오늘 22:22에 시작", "# 파묘"),
             PartyUiModel(R.drawable.img_party, "오늘 21:13에 시작", "# 왕과 사는 남자"),
             PartyUiModel(R.drawable.img_party, "오늘 22:22에 시작", "# 파묘"),
         )
