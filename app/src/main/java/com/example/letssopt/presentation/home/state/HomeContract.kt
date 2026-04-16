35435a58f40d2fef26eba31dc37fb95a9215eb32
seungjae708 <choesj365@gmail.com>
2026-04-16 19:28:15 +0900

    feat: HomeUiState 구현 및 더미 데이터 추가

---
+++ app/src/main/java/com/example/letssopt/presentation/home/state/HomeContract.kt
@@ -0,0 +1,31 @@
+package com.example.letssopt.presentation.home.state
+
+import com.example.letssopt.R
+import com.example.letssopt.presentation.home.model.PartyUiModel
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
+
+data class HomeUiState(
+    val bannerList: ImmutableList<Int> = persistentListOf(),
+    val hotList: ImmutableList<Int> = persistentListOf(),
+    val upcomingList: ImmutableList<Int> = persistentListOf(),
+    val partyList: ImmutableList<PartyUiModel> = persistentListOf()
+) {
+    companion object {
+        val dummyBannerList = listOf(
+            R.drawable.img_banner,
+            R.drawable.img_banner,
+            R.drawable.img_banner,
+        )
+        val dummyContentList = listOf(
+            R.drawable.img_content1,
+            R.drawable.img_content2,
+            R.drawable.img_content3,
+            R.drawable.img_content1,
+            )
+        val dummyPartyList = listOf(
+            PartyUiModel(R.drawable.img_party, "오늘 21:13에 시작", "# 왕과 사는 남자"),
+            PartyUiModel(R.drawable.img_party, "오늘 22:22에 시작", "# 파묘"),
+        )
+    }
+}
