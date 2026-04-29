df7a4aa0b2ea1e3356de8b8354a13b6b3267a311
seungjae708 <choesj365@gmail.com>
2026-04-29 18:18:38 +0900

    add: Room, Navigation, Serialization 및 의존성 번들 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -42,21 +44,29 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.lifecycle.viewmodel.compose)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
-    implementation(libs.kotlinx.immutable)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    dependencies {
+        // AndroidX Core & Lifecycle
+        implementation(libs.bundles.androidx.core)
+
+        // Compose
+        implementation(platform(libs.androidx.compose.bom))
+        implementation(libs.bundles.compose)
+        debugImplementation(libs.bundles.compose.debug)
+
+        // Navigation (SAA 적용)
+        implementation(libs.androidx.navigation.compose)
+
+        // Kotlinx
+        implementation(libs.kotlinx.immutable)
+        implementation(libs.kotlinx.serialization.json)
+
+        // Room
+        implementation(libs.bundles.room)
+        ksp(libs.androidx.room.compiler)
+
+        // Test
+        testImplementation(libs.junit)
+        androidTestImplementation(platform(libs.androidx.compose.bom))
+        androidTestImplementation(libs.bundles.androidTest)
+    }
 }
